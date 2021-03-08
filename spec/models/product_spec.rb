1d2a2fa5726d42762651d1db71470e1fe38e558d
Alon Rodovinsky <alon.rodovinsky@gmail.com>
2021-03-08 18:59:30 +0000

    all tests pass

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,34 +2,29 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
-    subject {
-      described_class.new(name: "Product",
-                  price: 100,
-                  quantity: 2,
-                  category: Category.find_by(id: 1)
-                  )
-      }
+    @category = Category.create(name: 'Toys')
+    product = Product.new(name: "Product", price: 100, quantity: 2, category: @category)
 
       it 'is valid with valid attributes' do
-        expect(subject).to be_valid
+        expect(product).to be_valid
       end
 
       it 'is not valid without a name' do
-        subject.name = nil
-        expect(subject).to_not be_valid
+        product.name = nil
+        expect(product).to_not be_valid
       end
 
       it 'is not valid without a price' do
-        expect(subject).to_not be_valid
+        expect(product).to_not be_valid
       end
 
       it 'is not valid without a quantity' do
-        expect(subject).to_not be_valid
+        expect(product).to_not be_valid
       end
 
       it 'is not valid without a category' do
         product.category = nil
-        expect(subject).to_not be_valid
+        expect(product).to_not be_valid
       end
   end
 end
