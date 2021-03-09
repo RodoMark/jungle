aa0fcc3e0d8669a32d93fc7f6d8797fc0b966cfa
Alon Rodovinsky <alon.rodovinsky@gmail.com>
2021-03-09 18:28:22 +0000

    all tests pass

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,15 @@
 class User < ActiveRecord::Base
-  validates :first_name,      presence: true
-  validates :last_name,       presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 
-  validates :email,           presence: true
-                              uniqueness: { case_sensitive: false,
-                              message: "should be a unique email" }
-                              length: { minumum: 6 }
+  validates :email, presence: true
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :email, length: { minimum: 6 }
 
   has_secure_password
 
   def authenticate_with_credentials(email, password)
-    user = User.find_by_email(email)
+    user = User.find_by_email(email.strip)
 
     if user && user.authenticate(password)
       return user
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'Validations' do
+    user = User.new(first_name: "First", last_name: "Last", email: "some@email.com", password: "password", password_confirmation: "password")
+
+      it 'is valid with valid attributes' do
+        expect(user).to be_valid
+      end
+
+      it 'is not valid without a first name' do
+        user.first_name = nil
+        expect(user).to_not be_valid
+      end
+
+      it 'is not valid without a last name' do
+        user.last_name = nil
+        expect(user).to_not be_valid
+      end
+
+      it 'is not valid without an email' do
+        user.email = nil
+        expect(user).to_not be_valid
+      end
+
+      it 'is not valid without a password' do
+        user.password = nil
+        expect(user).to_not be_valid
+      end
+  end
+
+  describe '.authenticate_with_credentials' do
+    user = User.create(first_name: "First", last_name: "Last", email: "thisemail@hasnotbeentaken.com", password: "password", password_confirmation: "password")
+
+    it 'should return nil if password is wrong' do
+    expect(user.authenticate_with_credentials(user.email, "wrongpassword")).to eq(nil)
+    end
+
+    it 'should return user even if the entered email has whitespace' do
+      expect(user.authenticate_with_credentials("  thisemail@hasnotbeentaken.com   ", "wrongpassword")).to eq(nil)
+      end
+
+    
+
+  end
+
 end
