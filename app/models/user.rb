8cb852c0b826b808b545892929fb503b1a40853e
Alon Rodovinsky <alon.rodovinsky@gmail.com>
2021-03-09 16:03:49 +0000

    add password length, first_name, and last_name validation to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,18 @@
 class User < ActiveRecord::Base
-  validates :email, uniqueness: { case_sensitive: false,
-    message: "should be a unique email" }
+  validates :first_name,      presence: true
+  validates :last_name,       presence: true
+
+  validates :email,           presence: true
+                              uniqueness: { case_sensitive: false,
+                              message: "should be a unique email" }
+                              length: { minumum: 6 }
+
   has_secure_password
+
+  def authenticate_with_credentials(mail, pass)
+    if
+      User.find_by(email: mail)
+    end
+  end
 
 end
